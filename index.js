100e9f85faf693e68b01d9020a435a71723a840d
Jose DlhB <josehoz.ing@gmail.com>
2023-05-13 10:21:17 -0500

    Comentarios, variables, notación de variables y constantes

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-alert('Hola Mundo');
+// Hola Mundo
+
+//alert('Hola Mundo');
+
+//Comentarios de línea
+/*
+Comentarios de más de una línea
+*/
+
+// Variables
+
+let a = 5; //Declarar variables, las variables no son tipadas
+let b = 5;
+let suma = a + b;
+console.log("El resultado de la suma es: "+ suma);
+
